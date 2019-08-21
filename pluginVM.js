44e3e394bdb2bcc81ec4dcbf99ce619afc8a5eb9
Victor Machado <joelvictor2012@gmail.com>
2019-08-20 22:02:05 -0300

    Update separating conditionals

--- pluginVM.js
+++ pluginVM.js
@@ -1,76 +1,89 @@
 function PluginVM() {
+  this.onInit = function() {
+    const button = document.createElement("button"),
+      div = document.createElement("div");
 
-    this.onInit = function () {
-        const button = document.createElement('button'),
-            div = document.createElement('div');
+    button.innerHTML = "Executar plugin";
 
-        button.innerHTML = "Executar plugin";
+    button.addEventListener("click", PluginVM.makeChanges);
 
-        button.addEventListener("click", PluginVM.makeChanges);
+    button.className += "btn btn-danger";
 
-        button.className += "btn btn-danger";
+    button.setAttribute("id", "buttonPluginVM");
 
-        button.setAttribute("id", "buttonPluginVM");
+    button.style.position = "fixed";
+    button.style.zIndex = "2";
+    button.style.bottom = "10px";
+    button.style.fontFamily = "Arial";
 
-        button.style.position = "fixed";
-        button.style.zIndex = "2";
-        button.style.bottom = "10px";
-        button.style.fontFamily = "Arial";
+    div.style.width = "100%";
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
 
-        div.style.width = "100%";
-        div.style.display = "flex";
-        div.style.justifyContent = "center";
+    document.body.appendChild(div);
+    div.appendChild(button);
+  };
 
-        document.body.appendChild(div);
-        div.appendChild(button);
-    }
+  this.makeChanges = function() {
+    var aux,
+      temp,
+      filterFunction,
+      filteredTree,
+      textNodes = [];
 
-    this.makeChanges = function () {
-        var aux, temp, filterFunction, filteredTree, textNodes = [];
+    filterFunction = function(node) {
+      return (node.nodeType === 3 &&
+        node.textContent.trim() !== "" &&
+        node.parentNode.id != "buttonPluginVM") ||
+        node.tagName == "IMG"
+        ? NodeFilter.FILTER_ACCEPT
+        : NodeFilter.FILTER_SKIP;
+    };
 
-        filterFunction = function (node) {
-            return (node.nodeType === 3 && node.textContent.trim() !== '' && node.parentNode.id != 'buttonPluginVM') || node.tagName == "IMG" ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+    temp = /MSIE|Trident/.test(navigator.userAgent)
+      ? filterFunction
+      : {
+          acceptNode: filterFunction
         };
 
-        temp = /MSIE|Trident/.test(navigator.userAgent) ? filterFunction : {
-            acceptNode: filterFunction
-        };
+    filteredTree = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_ALL,
+      temp,
+      false
+    );
 
-        filteredTree = document.createTreeWalker(document.body, NodeFilter.SHOW_ALL, temp, false);
+    while ((aux = filteredTree.nextNode())) textNodes[textNodes.length] = aux;
 
-        while (aux = filteredTree.nextNode())
-            textNodes[textNodes.length] = aux;
+    for (var i = textNodes.length - 1; i >= 0; i--) {
+      const element = textNodes[i];
 
-        for (var i = textNodes.length - 1; i >= 0; i--) {
-            const element = textNodes[i];
+      setFontSizeAndFamily(element.parentNode);
 
-            setFontSizeAndFamily(element.parentNode);
+      if (element.tagName === "A") setHighlightsInLinks(element.parentNode);
 
-            setHighlightsInLinks(element.parentNode);
-
-            setHighlightsInImages(element);
-        }
-
+      if (element.tagName == "IMG" && !element.getAttribute("alt"))
+        setHighlightsInImages(element);
     }
+  };
 
-    function setFontSizeAndFamily(element) {
-        element.style.setProperty('font-size', parseFloat(getComputedStyle(element).fontSize) * 1.2 + 'px');
-        element.style.setProperty('font-family', 'Arial');
-    }
+  function setFontSizeAndFamily(element) {
+    element.style.setProperty(
+      "font-size",
+      parseFloat(getComputedStyle(element).fontSize) * 1.2 + "px"
+    );
+    element.style.setProperty("font-family", "Arial");
+  }
 
-    function setHighlightsInLinks(element) {
-        if (element.tagName === 'A') {
-            element.style.setProperty('background-color', '#FFE599');
-            element.style.setProperty('text-decoration', 'underline');
-            element.style.setProperty('font-style', 'italic');
-        }
-    }
+  function setHighlightsInLinks(element) {
+    element.style.setProperty("background-color", "#FFE599");
+    element.style.setProperty("text-decoration", "underline");
+    element.style.setProperty("font-style", "italic");
+  }
 
-    function setHighlightsInImages(element) {
-        if (element.tagName == "IMG" && !element.getAttribute('alt'))
-            element.style.setProperty('border', '5px dotted red');
-    }
-
+  function setHighlightsInImages(element) {
+    element.style.setProperty("border", "5px dotted red");
+  }
 }
 
 var PluginVM = new PluginVM();
